46357df1ab113dbb463826910d7521c13f310101
Ko <mahadevihik0@gmail.com>
2026-04-24 18:30:36 +0530

    Update login URL for MajorLogin endpoint

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ async def create_jwt(region: str):
     body = json.dumps({"open_id": open_id, "open_id_type": "4", "login_token": token_val, "orign_platform_type": "4"})
     proto_bytes = await json_to_proto(body, FreeFire_pb2.LoginReq())
     payload = aes_cbc_encrypt(MAIN_KEY, MAIN_IV, proto_bytes)
-    url = "https://loginbp.ggblueshark.com/MajorLogin"
+    url = "https://loginbp.ggpolarbear.com/MajorLogin"
     headers = {
         'User-Agent': USERAGENT, 'Connection': "Keep-Alive", 'Accept-Encoding': "gzip",
         'Content-Type': "application/octet-stream", 'Expect': "100-continue",
